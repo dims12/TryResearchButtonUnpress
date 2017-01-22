e623bd5a28418bd5c091cbf22e4fd9ee52b8b12c
dims12@mail.ru <ltdeirf88>
2017-01-22 22:27:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Try to Research Button Unpress
+
+This projects shows, that a button, which pressed state is bound to observable, after some
+time goes unpressed itself.
+
---
+++ app/src/main/java/com/inthemoon/tryresearchbuttonunpress/TryResearchButtonUnpress.java
@@ -0,0 +1,22 @@
+package com.inthemoon.tryresearchbuttonunpress;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ObservableBoolean;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inthemoon.tryresearchbuttonunpress.databinding.ActivityTryResearchButtonUnpressBinding;
+
+public class TryResearchButtonUnpress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.activity_try_research_button_unpress);
+        ActivityTryResearchButtonUnpressBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_try_research_button_unpress);
+        binding.setActivity(this);
+    }
+
+    public final ObservableBoolean pressed = new ObservableBoolean(true);
+}
